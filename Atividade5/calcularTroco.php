40e2ef31546518f215d896008bddf4f183116db6
Bruno Cunha <rocks.bn@gmail.com>
2017-09-16 17:08:55 +0000

    Add os comentarios no codigo

--- Atividade5/calcularTroco.php
+++ Atividade5/calcularTroco.php
@@ -1,25 +1,35 @@
 <?php
 
+/* Esta funcao calcula o troco de forma recursviva
+    * @param {int} $troco - o valor restante que deve dar de troco  
+    * @param {int} $valorNota - o valor a nota para calcular quantos dessa nota deve dar de troco
+    * @return {int} a quantidade de notas que deve ser entregue ao cliente
+*/
 function calcularTrocoRecursivo($troco, $valorNota){
     
-    if ($troco == 0){
-        return 0;
+    if ($troco == 0){ //se troco for 0, chegamos ao final da recursão
+        return 0;     //retorna 0
     }
-    else{
-        $qtdNota = floor($troco/$valorNota);
-        echo "Qtd notas de R$" . $valorNota . ": " . $qtdNota . "<br/>";
-        if ($valorNota == 100)
+    else{  //se nao for 0, FAÇA:
+        //calcula a quantidade de notas que deve ser entregue foi utiizado a função "FLOOR", caso o resultado tenha ponto flutuante
+        $qtdNota = floor($troco/$valorNota); 
+        echo "Qtd notas de R$" . $valorNota . ": " . $qtdNota . "<br/>"; //exibe na tela a quantidade de notas
+        
+        /* calcularemos a proxima nota*/
+        if ($valorNota == 100) //se a nota for 100, a proxima nota sera 50
             return calcularTrocoRecursivo($troco % $valorNota, 50);
-        else if ($valorNota == 50)
+        else if ($valorNota == 50) //se a nota for 50, a proxima nota sera 20
             return calcularTrocoRecursivo($troco % $valorNota, 20);
-        else if ($valorNota == 20)
+        else if ($valorNota == 20) //se a nota for 20, a proxima nota sera 10
             return calcularTrocoRecursivo($troco % $valorNota, 10);
-        else if ($valorNota == 10)
+        else if ($valorNota == 10) //se a nota for 10, a proxima nota sera 5
             return calcularTrocoRecursivo($troco % $valorNota, 5);
-        else if ($valorNota == 5)
+        else if ($valorNota == 5) //se a nota for 5, a proxima nota sera 1
             return calcularTrocoRecursivo($troco % $valorNota, 1);
     }
 }
+
+
 $valorTotal = $_POST["totalCompra"];
 $valorPago  = $_POST["valorPago"];
 
@@ -32,39 +42,7 @@ else if ($valorTotal == $valorPago){
 else{
     $troco = $valorPago - $valorTotal;
     echo "Troco: R$" . $troco . "<br/>";
-    
-    
     calcularTrocoRecursivo($troco, 100);
-    /*$falta = 0;
-    
-    
-    
-    $notas_100 = floor($trocoI/100);
-    $troco = $troco % 100;
-    
-    $notas_50 = floor($troco/50);
-    $troco = $troco % 50;
-    
-    $notas_20 = floor($troco/20);
-    $troco = $troco % 20;
-    
-    $notas_10 = floor($troco/10);
-    $troco = $troco % 10;
-    
-    $notas_5 = floor($troco/5);
-    $troco = $troco % 5;
-    
-    $notas_1 = floor($troco/1);
-    $troco = $troco % 1;
-    
-    echo "Troco: R$" . $trocoI . "<br/>";
-    echo "Qtd. notas de R\$100:  " . $notas_100 . "<br/>";
-    echo "Qtd. notas de R\$50:   " . $notas_50 . "<br/>";
-    echo "Qtd. notas de R\$20:   " . $notas_20 . "<br/>";
-    echo "Qtd. notas de R\$10:   " . $notas_10 . "<br/>";
-    echo "Qtd. notas de R\$5:   "  . $notas_5 . "<br/>";
-    echo "Qtd. notas de R\$1:   "  . $notas_1 . "<br/>";*/
-    
 }
 
 
