22d5e8f7669c58d259346d54ee6626823cc04ae4
Bruno Cunha <rocks.bn@gmail.com>
2017-09-16 14:33:51 +0000

    Nome da pasta corrigda de Atividade6 para Atividade4

--- Atividade6/calcularMedia.php
+++
@@ -1,9 +0,0 @@
-<?php
-$str_valores    = $_POST["valores"];            //pega os valores do formulário pelo metodo POST
-$array_valores  = explode(",", $str_valores);   //divide a string em um array
-
-$media = array_sum($array_valores)/count($array_valores);   //faz o cálculo da média (soma o array / qtd de valores no array)
-
-echo "A media dos valores " . $str_valores . "<br/>";   //exibe o resultado
-echo "<h1>= " . $media . "</h1>";                       //exibe o resultado
-?>
